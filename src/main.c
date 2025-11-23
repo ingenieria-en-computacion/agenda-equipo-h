7dd32703dadc82431214e259a8a844ef8f8d8190
Shirell <shirellcline@gmail.com>
2025-11-22 18:43:30 -0600

    prueba

--- src/main.c
+++ src/main.c
@@ -3,3 +3,13 @@
 #include <string.h> 
 #include "agenda.h" 
 
+void mostrar_menu(){
+    printf("\nAgenda:\n");
+    printf("1. Agregar un nuevo contacto\n");
+    printf("2. Buscar un contacto por nombre\n");
+    printf("3. Mostrar agenda\n");
+    printf("4. Guardar contactos en archivo\n");
+    printf("5. Cargar los contactos desde archivo\n");
+    printf("6 . salir\n");
+    
+}
