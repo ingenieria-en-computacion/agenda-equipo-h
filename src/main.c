35b6957ccb3d224a0f26fc023982311a4ef8c8b9
U-LAPTOP-6N34HVB7\Ian Sanchez <iansanchezgomez83@gmail.com>
2025-11-22 18:54:03 -0600

    Lo que sea

--- src/main.c
+++ src/main.c
@@ -13,3 +13,65 @@ void mostrar_menu(){
     printf("6 . salir\n");
     
 }
+
+int main(){
+    Agenda agenda;
+    iniciar_agenda(&agenda);
+
+    int opcion;
+    char nombre[30];
+    char telefono[12];
+    char archivo[100];
+
+    do{
+        mostrar_menu();
+        scanf("%d",&opcion);
+        switch(opcion){
+            case1:{
+                Contacto c;
+                printf("Agregar contacto\n");
+                leer_contacto(&c);
+                agregar_contacto(&agenda,c);
+                break;
+            }
+            case 2:{
+                printf("Ingrese el nombre que quiere buscar:");
+                scanf("%s",nombre);
+                int pos = buscar_contacto(&agenda, nombre);
+                if(pos == -1)printf(" Error fatal, no se encuentra el contacto\n");
+                else mostrar_contacto(agenda.contactos[pos]);
+                break;
+            }
+            case 3:
+            printf("Agenda:)");
+            imprimir_agenda(agenda);
+            break;
+
+            case 4:
+                printf("Nombre del archivo a guardar: ");
+                scanf("%s", archivo);
+                guardar_contactos(archivo,*&agenda*);
+                printf("Contactos guardados.\n");
+                break;
+            
+            case 5: 
+                printf("Nombre del archivo a cargar: ");
+                scanf("%s", archivo);
+                guardar_contactos(archivo, &agenda);
+                printf("Contactos cargados.\n");
+                break;
+            
+            case 6:
+            printf("ha salido de su agenda");
+            break;
+
+            default:
+                printf("error fatal");
+    
+        }
+
+
+    }while (opcion !=9);
+    
+return 0;
+}
